b3a2736ea220eb49c2aa7b4cf8e55f20af7c28c0
Lars <lars@consultux.de>
2019-01-06 23:13:10 -0500

    v2

--- garager/garager.lua
+++ garager/garager.lua
@@ -1,28 +1,23 @@
-print('executing temponly.lua')
+print('executing garager.lua')
 
-sda = 6 -- SDA Pin
-scl = 7 -- SCL Pin
-ds=2
-trig=0
-echo=5
-signal=4
-button=8
+SDA = 6 -- SDA Pin
+SCL = 7 -- SCL Pin
+DS=2
+TRIG=0
+ECHO=5
+SIGNAL=4
+BUTTON=8
 
-door_treshold=40
+--FIXME3: read from config
+DOOR_TRESHOLD=40
 
+sensor.temp=0
+sensor.door=-1
+sensor.dist=0
+sensor.door_status=0
 
-temp=0
 start=0
 done=0
-state=0
-prior=10000
-door_open=0
-door=""
-dist=0
-tsensor=nil
-
-problems=0
-
 
 function init_display(sda,scl) --Set up the u8glib lib
      local sla = 0x3C
@@ -42,150 +37,144 @@ function updateDisp(text)
     disp:setFont(u8g2.font_6x10_tf)
     disp:drawStr(5,1,"Status: "..status)
     disp:setFont(u8g2.font_unifont_t_symbols)
---    disp:drawStr(5,15, "Temp: "..temp)
-    disp:drawStr(5,10,"Door: "..door)
+    local dstr=""
+    if sensor.door==1 then
+        dstr = "OPEN"
+    elseif sensor.door==0 then
+        dstr = "CLOSED"
+    else
+        dstr = "UNKNOWN"
+    end
+    disp:drawStr(5,10,"Door: "..dstr)
     disp:setFont(u8g2.font_6x10_tf)
-    disp:drawStr(5,22,"Temp: "..temp)
+    disp:drawStr(5,22,"Temp: "..sensor.temp)
     disp:sendBuffer()
 end
 
 function readTemp2()
-    tsensor:read_temp(readout, ds)
+    tsensor:read_temp(readout, DS)
     --print(temp)
-    updateDisp("")
 end
 
-function readout(te)
-  for addr, tem in pairs(te) do
+function readout(temp)
+  local emtpy=true
+  for addr, tem in pairs(tsensor.temp) do
     --print(string.format("Sensor %s: %s °C", ('%02X:%02X:%02X:%02X:%02X:%02X:%02X:%02X'):format(addr:byte(1,8)), tem))
-    if not tem==0 then
-        temp=tem
-        -- FIXME: after n failed attempts, unset CAN_TEMP
+    if tem ~= "0.0" then
+        sensor.temp=tem
+        status=bit.set(status, CAN_TEMP)
+        empty=false
+        updateDisp("")
+    else
+        print("read 0.0 temperature")
+    end
+    if empty == true then
+        print("can't read temp sensor")
+        status=bit.clear(status, CAN_TEMP)
     end
   end
 end
 
 function echo_cb(level, when)
      --print("callback", level)
-     if level == 1 then
-          --print("callback up", level, when)
-          start=when
-          gpio.trig(echo, "down")
-     else
-          done=when
-          --print("callback down", level, when)
-     end
+    if level == 1 then
+        --print("callback up", level, when)
+        start=when
+        gpio.trig(ECHO, "down")
+    else
+        --print("callback down", level, when)
+        sensor.dist = (when-start) / 58
+    end
 end
 
 function measure_distance()
      --print("trying to measure distance")
-     gpio.trig(echo, "both", echo_cb)
-     gpio.write(trig, gpio.HIGH)
+     gpio.trig(ECHO, "both", echo_cb)
+     gpio.write(TRIG, gpio.HIGH)
      tmr.delay(100)
-     gpio.write(trig, gpio.LOW)
+     gpio.write(TRIG, gpio.LOW)
      tmr.alarm(1,200, tmr.ALARM_SINGLE, distance)  
 end
 
 function distance()
-   dist = (done-start) / 58
-   done=start
-   --print(dist)
-
-   if dist > 0 then 
-     --print("distance:", distance) 
-     status=bit.set(status, CAN_DISTANCE)
-     tmr.alarm(2,180000,1,mqtt_publish)
-     if dist < door_treshold then
-          -- door open!
-          if door_open == 0 then 
+    local reading
+    if sensor.dist <= 0 then 
+        reading = -1
+        status=bit.clear(status, CAN_DISTANCE)
+    elseif sensor.dist < DOOR_TRESHOLD then
+        reading = 1
+        status=bit.set(status, CAN_DISTANCE)
+    else
+        reading =0
+        status=bit.set(status, CAN_DISTANCE)
+    end
+    if sensor.door_status > 0 then
+        -- this is a second read after a change in status
+        --print("read distance after change in status, now "..reading)
+        sensor.door_status = bit.set(sensor.door_status, reading + 4)
+        if (sensor.door_status % 9) == 0 then
+            --print("change in status detected, now "..reading)
+            sensor.door = reading
             tmr.alarm(1,20,0,report_change)
-          end
-          door_open=1
-     else
-          if door_open == 1 or prior > 100 then 
-            tmr.alarm(1,20,0,report_change)
-          end
-          door_open=0
-     end     
-   else
-       status=bit.clear(status, CAN_DISTANCE)
-       tmr.stop(2)
-   end
-   prior=0
+        end
+        sensor.door_status = 0
+    elseif reading ~= sensor.door then
+        -- this is a first discovery of a status change
+        sensor.door_status = bit.set(sensor.door_status, reading + 1)
+        --print("possible status change detected, reading again to be sure "..reading)
+    end
 end
 
 function report_change()
-    if door_open == 1 then 
+    pcall(mqtt_publish)
+    if sensor.door == 1 then 
         print("door open!")
-        door="OPEN"
-        -- notify state change?
-        gpio.write(signal, gpio.LOW)
-        updateDisp()
+        --FIXME3: move setting text of door into updateDisp(), eliminate global variable?
+        gpio.write(SIGNAL, gpio.LOW)
     else
         print("door closed!")
-        door="CLOSED"
-        -- notify state change?
-        gpio.write(signal, gpio.HIGH)
-        updateDisp()
-       -- tmr.alarm(5,300, tmr.ALARM_SINGLE, measure)
+        gpio.write(SIGNAL, gpio.HIGH)
     end
-    mqtt_publish()
+    updateDisp()
 end
 
 function button_press()
-      gpio.write(button,1)
+      gpio.write(BUTTON,1)
       tmr.alarm(6,1500,0, button_release)
 end
 
 function button_release()
-      gpio.write(button,0)
-end
-
-function loop()
-    measure_distance()
-    --readMotion()
-    tmr.alarm(4,100,0,updateDisp)
+      gpio.write(BUTTON,0)
 end
 
 function mqtt_publish()
-    local msg = '{"status":"'..status..'","door":"'..door_open..'","temp":"'..temp..'","problems":"'..problems..'"}'
-    local ret = mq:publish(topic.."/status", msg,1,0, function(client)
-        print("data sent via mqtt")
-    end)
-    if ret == true then
-        status = bit.set(status, CAN_PUBLISH)
-    else
+    update_sensor()
+    if not pcall(function() mq:publish(topic.."/status", sjson.encode(sensor),1,0, function(client)
+            print("data sent via mqtt")
+            status = bit.set(status, CAN_PUBLISH)
+            dog=tmr.softwd(WATCHDOG)
+        end)
+    end) then
         status = bit.clear(status, CAN_PUBLISH)
-        problems = problems + 1
-        if problems==10 then 
-            node.restart()
-        end
+        log("mqtt_publish failed")
     end
 end
 
-function init_temp()
-    tsensor=require("ds18b20")
-end
---ds18b20.setup(ds)
 
-if pcall(init_temp) then
-    print("loaded ds18b20 module")
-    status=bit.set(status, CAN_TEMP)
-end
-gpio.mode(signal, gpio.OUTPUT)
---gpio.mode(button, gpio.OUTPUT)
---gpio.write(signal, gpio.LOW)
---gpio.write(button, gpio.LOW)
-gpio.mode(trig,gpio.OUTPUT)
-gpio.mode(echo,gpio.INT)
---gpio.mode(motion_pin, gpio.INPUT)
--- turn off builtin LED
---gpio.write(4,gpio.HIGH)
+gpio.mode(SIGNAL, gpio.OUTPUT)
+--gpio.mode(BUTTON, gpio.OUTPUT)
+--gpio.write(SIGNAL, gpio.LOW)
+--gpio.write(BUTTON, gpio.LOW)
+gpio.mode(TRIG,gpio.OUTPUT)
+gpio.mode(ECHO,gpio.INT)
 
-init_display(sda,scl)
+init_display(SDA,SCL)
+init_display=nil
 
 if bit.isset(status, CAN_TEMP) then
-    tmr.alarm(3,3031,1,readTemp2)
+    readTemp2()
+    tmr.alarm(3,5031,1,readTemp2)
 end
-tmr.alarm(5,2052,1,loop)
-
+tmr.alarm(5,2052,1,measure_distance)
+tmr.alarm(2,180000,1,mqtt_publish)
+ 
--- garager/init.lua
+++ garager/init.lua
@@ -1,15 +1,63 @@
-status=0
 WIFI=1
-MQTT=2
-LATEST=3
-CAN_DISTANCE=4
-CAN_PUBLISH=5
-CAN_TEMP=6
+LATEST=2
+MQTT=3
+CAN_PUBLISH=4
+INIT=5
+CAN_DISTANCE=6
+CAN_TEMP=7
+-- FIXME: read timers and other config from config file
+WATCHDOG=300000
+
+--FIXME1: when to store status in rtcmem.write and load as prior last status
+
+--FIXME2: improve log handling: ability to suppress debug
+
+status=0
+dog=tmr.softwd(WATCHDOG)
 
 topic,_=string.gsub(wifi.sta.getmac(), ":","")
 _ = nil
 mq=mqtt.Client(topic, 120)
 topic="/esp/"..topic
+lstack ={}
+srv=net.createServer(net.TCP)
+
+tsensor=nil
+sensor={}
+
+function init_finished()
+    status=bit.set(status, INIT)
+    startup = nil
+    dofile("garager.lua")
+end
+
+function update_sensor()
+    sensor.memtot, sensor.memused = node.egc.meminfo()
+    sensor.heap = node.heap()
+    sensor.status = status
+    local str=""
+    if lstack[1] then
+        str = lstack[1]
+    end
+    sensor.lastlog = str
+end
+
+function log(str)
+    table.insert(lstack, str)
+    print(str)
+    update_sensor()
+    --FIXME2: error handling here?
+    local function send_log()
+        mq:publish(topic.."/log", sjson.encode(sensor),0,0, function(client)
+            status=bit.set(status, CAN_PUBLISH)
+            table.remove(lstack, 1)            
+        end)
+    end
+        -- try to send other pending log messages
+    if #lstack > 0 then
+        send_log()
+    end
+end
 
 function startup()
     if not bit.isset(status, WIFI) then
@@ -21,62 +69,74 @@ function startup()
         end
         tmr.alarm(0,200,0, startup)
     else
+        if pcall(init_temp) then
+            print("loaded ds18b20 module")
+            status=bit.set(status, CAN_TEMP)
+            init_temp=nil
+        else
+            log("couldn't load ds18b20 module")
+        end    
+        srv=net.createServer(net.TCP)
+        srv:listen(81, srv_message)
+        --FIXME: add srv to status bitmask?
+        print("backdoor srv started")
         if not bit.isset(status, MQTT) then
             mq:lwt(topic.."/available", "offline", 2)
             mqtt_connect()
         end
     end
-    --dofile("temponly.lua")
 end
 
 function update()
     -- FIXME: Updater logic!
     http.get("http://192.168.1.85"..topic.."/updater.lua", nil, function(code, data)
         if (code ~= 200) then
-            print("Updater HTTP request failed")
-            dofile("temponly.lua")
+            log("Updater HTTP request failed")
         else
-            print("Updater: "..code, data)
+            log("Updater: "..code)
             fd = file.open("updater.lua", "w")
             if fd then
-              -- write 'foo bar' to the end of the file
               fd:write(data)
               fd:close()
+              -- FIXME1: pcall error handling
+              f=loadfile("updater.lua")
+              f()
               status=bit.set(status, LATEST)
-              dofile("temponly.lua")
+              log("updater ran successfully")
             else
-                print('could not write updater file')
-                dofile("temponly.lua")
+                log('could not write or run updater file')
             end
         end
     end)
 end
 
 function mqtt_connect()
-    startup = nil
     print("connecting to MQTT")
     if pcall(mq:connect("192.168.1.31", 1883, 0, 0, function(conn)
+        log(lastmsg)
         print ("Connected to MQTT")
-        mq:publish(topic.."/available", "online",2,0)
+        mq:publish(topic.."/available", "online",1,0)
         conn:subscribe(topic.."/command", 0, function(client) 
-            print("subscribe success") 
+            log("subscribe success") 
         end)
         status=bit.set(status, MQTT)
-        update()
+        dog=tmr.softwd(WATCHDOG)
+        if not bit.isset(status, INIT) then
+            init_finished()
+        end
     end)) then
-        print ("unexpected mqtt error, trying again in 5 sec")
+        log ("unexpected mqtt error, trying again in 5 sec")
         tmr.alarm(0,5000,0,mqtt_connect)
     end
 end
 
 mq:on("offline", function(client) 
-    print ("MQTToffline")
+    --print ("MQTToffline")
     status=bit.clear(status, MQTT) 
-    -- FIXME: try to reconnect?
-    node.restart()
+    log("reconnecting mqtt")
+    tmr.alarm(0,1000,0,mqtt_connect)
 end)
 
--- on publish message receive event
 mq:on("message", function(client, topic, data) 
     print(topic .. ": " ) 
     if data ~= nil then
@@ -91,20 +151,56 @@ mq:on("message", function(client, topic, data)
     end
 end)
 
--- on publish overflow receive event
 mq:on("overflow", function(client, topic, data)
-    print(topic .. " partial overflowed message: " .. data )
+    log(topic .. " partial overflowed message: " .. data )
 end)
 
+function srv_message(conn)
+    conn:on("receive", function(sck, data)
+        data = string.gsub(data, "%c","")
+        local port, ip = sck:getpeer()
+        log("srv received from "..ip..": "..data)
+        if data == "status" then
+            update_sensor()
+            sck:send(sjson.encode(sensor))
+        elseif data == "ping" then
+            log("responding to srv ping")
+            sck:send("triggering mqtt_publish\n")
+            pcall(mqtt_publish)
+        elseif data == "restart" then
+            sck:send("restarting in a few seconds\n")
+            local t=tmr.create()
+            t:register(2000, tmr.ALARM_SINGLE, node.restart)
+            t:start()
+        elseif data == "update" then
+            sck:send("starting update in a few seconds\n")
+            local t=tmr.create()
+            t:register(2000, tmr.ALARM_SINGLE, update)
+            t:start()
+        else
+            sck:send("thanks for "..data.."\n")
+        end
+    end) 
+    
+    conn:on("sent", function() 
+        conn:close() 
+        print("closed connection")
+        conn=nil
+    end)
+
+    conn:on("connection", function(conn) 
+        print("new connection")
+    end)
+
+end
+
+function init_temp()
+    tsensor=require("ds18b20")
+end
+
+print("config wifi")
 print("You have 5 seconds to abort")
 print("Waiting...")
 status=1
-print("config wifi")
 wifi.setmode(wifi.STATION)
-local cfg ={};
-cfg.ssid="DELETED";
-cfg.pwd="DELETED";
-cfg.save=true;
-wifi.sta.config(cfg)
-cfg = nil
 tmr.alarm(0,5000,0, startup)
